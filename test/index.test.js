e8503b16dcf7ea19e2cd4aabb661e685cf321007
prathusingh <prathusingh21@gmail.com>
2022-12-18 08:03:56 -0500

    feat: addedd tests

---
+++ test/index.test.js
@@ -0,0 +1,43 @@
+const { tmpdir } = require("os")
+const { join } = require("path")
+const { writeFileSync } = require("fs")
+const { envsLoader } = require("../src/index")
+
+describe("envsLoader", () => {
+  let tempDir = ""
+  let fileA = ""
+  let fileB = ""
+
+  let env = process.env
+
+  beforeEach(() => {
+    process.env = {}
+    tempDir = tmpdir()
+
+    fileA = join(tempDir, "fileA.env")
+    fileB = join(tempDir, "fileB.env")
+
+    writeFileSync(fileA, "EXAMPLE_A=apple\nEXAMPLE_B=bat")
+    writeFileSync(fileB, "EXAMPLE_A=astronaut\nEXAMPLE_C=cat")
+  })
+
+  afterEach(() => {
+    process.env = env
+  })
+
+  it("works with one env", () => {
+    envsLoader(fileA)
+
+    expect(process.env["EXAMPLE_A"]).toEqual("apple")
+    expect(process.env["EXAMPLE_B"]).toEqual("bat")
+    example(process.env["EXAMPLE_C"]).toBeUndefined()
+  })
+
+  it("works with multiple env", () => {
+    envsLoader(fileA, fileB)
+
+    expect(process.env["EXAMPLE_A"]).toEqual("astronaut")
+    expect(process.env["EXAMPLE_B"]).toEqual("bat")
+    example(process.env["EXAMPLE_C"]).toEqual("cat")
+  })
+})
