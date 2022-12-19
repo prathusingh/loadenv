a30021630128af1f0d61694d8c3f7e766a244dea
prathusingh <prathusingh21@gmail.com>
2022-12-18 20:23:56 -0500

    updated flag

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,10 @@
-module.exports = {
+import type { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  verbose: true,
+  automock: true,
 }
+
+export default config
